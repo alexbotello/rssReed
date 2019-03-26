bb5039d0e3c40e40f5888e6e4acbaf58bb7328ff
Alexander <alexander.botello@g.austincc.edu>
2019-03-26 14:40:02 -0500

    Add templates for the time being to see websocket in action

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	items := getAllRecords()
+
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, items)
+}
+
 func main() {
-	verifyDatabase()
-	getAllRecords()
-	runProcess()
+	stream := newStream()
+	verifyDatabase(stream)
+
+	http.Handle("/", &templateHandler{filename: "rss.html"})
+	http.Handle("/stream", stream)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
