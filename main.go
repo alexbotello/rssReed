9f03d690a358344fae57894d0e4b6a8ed63a5bf8
Alexander <alexander.botello@g.austincc.edu>
2019-03-26 15:53:42 -0500

    Use /exit route to handle shutdown and deletion of backend

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
@@ -31,6 +32,14 @@ func main() {
 
 	http.Handle("/", &templateHandler{filename: "rss.html"})
 	http.Handle("/stream", stream)
+
+	http.HandleFunc("/exit", func(w http.ResponseWriter, r *http.Request) {
+		err := os.Remove("rss.db")
+		if err != nil {
+			log.Fatal("Failed to remove database upon close")
+		}
+		os.Exit(1)
+	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
