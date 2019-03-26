37408c1afe8b1ecfbffdd6318f17c4cd9be87657
Alexander <alexander.botello@g.austincc.edu>
2019-03-25 22:16:28 -0500

    Add CRUD database helper functions

--- db.go
+++ db.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"sort"
+
+	"github.com/jinzhu/gorm"
+	"github.com/mmcdole/gofeed"
 )
 
 func verifyDatabase() {
@@ -14,4 +19,29 @@ func verifyDatabase() {
 		defer f.Close()
 	}
 	return
+}
+
+func addItemToDB(db *gorm.DB, item *gofeed.Item) {
+	var rI RssItem
+	if result := db.Where(&RssItem{Title: item.Title}).First(&rI); result.Error != nil {
+		rI = RssItem{
+			Title: item.Title,
+			Link:  item.Link,
+			Desc:  item.Description,
+			Date:  item.PublishedParsed,
+		}
+		db.NewRecord(rI)
+		db.Create(&rI)
+		fmt.Println("Adding item into DB")
+	}
+}
+
+func getAllRecords(db *gorm.DB) {
+	var items []RssItem
+	db.Find(&items)
+	sort.Sort(byTime(items))
+	for _, item := range items {
+		// fmt.Println(item.Date.Format("Mon Jan _2 15:04:05 2006"))
+		fmt.Println(item.Title)
+	}
 }
