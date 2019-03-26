077880bb85aa2cb13021122ea6c1fcf93e3528f2
Alexander <alexander.botello@g.austincc.edu>
2019-03-25 22:18:36 -0500

    Establish db connection, table migration/creation

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
 func main() {
-	runProcess()
+	verifyDatabase()
+	db, err := gorm.Open("sqlite3", "rss.db")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&RssItem{})
+	getAllRecords(db)
+	runProcess(db)
 }
