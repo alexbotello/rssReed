e55f6bad3be07fbb448b7aaea41dbf8b304f62e0
Alexander <alexander.botello@g.austincc.edu>
2019-03-26 20:48:46 -0500

    Add jsonHandler and exitHandler for use with Electron client

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -26,22 +27,35 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.templ.Execute(w, items)
 }
 
+type jsonHandler struct {
+	items []RssItem
+}
+
+func (j *jsonHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	j.items = getAllRecords()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(j.items)
+}
+
+type exitHandler struct{}
+
+func (e *exitHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := os.Remove("rss.db")
+	if err != nil {
+		log.Fatal("Failed to remove database upon close")
+	}
+	os.Exit(1)
+}
+
 func main() {
 	stream := newStream()
 	verifyDatabase(stream)
 
-	http.Handle("/", &templateHandler{filename: "rss.html"})
+	http.Handle("/", &jsonHandler{})
 	http.Handle("/stream", stream)
+	http.Handle("/exit", &exitHandler{})
 
-	http.HandleFunc("/exit", func(w http.ResponseWriter, r *http.Request) {
-		err := os.Remove("rss.db")
-		if err != nil {
-			log.Fatal("Failed to remove database upon close")
-		}
-		os.Exit(1)
-	})
-
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":5000", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
