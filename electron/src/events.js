7fb910a2ad772a721d92c8316934c6646cdde1e8
Alexander <alexander.botello@g.austincc.edu>
2019-03-28 10:49:48 -0500

    Watch container for mousover to remove newItem class once item has been visited

--- electron/src/events.js
+++ electron/src/events.js
@@ -1,7 +1,7 @@
 $(function () {
-    $(".rssItem").hover(
-        function () {
-          $(this).removeClass("newItem");
-        }
-    );
+    // Remove green border when new item is touched
+    let container =  document.getElementById("container")
+    container.addEventListener("mouseover", (event) => {
+        event.target.classList.remove("newItem")
+    })
 });
