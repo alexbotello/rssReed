e0f4bb8d5d793b1efea79ee28c8617d02c3ae943
Alexander <alexander.botello@g.austincc.edu>
2019-03-28 13:52:12 -0500

    Fetch / server route on document load

--- electron/src/events.js
+++ electron/src/events.js
@@ -1,7 +1,32 @@
 $(function () {
+    fetch("http://localhost:5000/")
+        .then(resp => {
+            resp.json()
+                .then(data => {
+                    console.log(data)
+                    let feeds = document.getElementById("feeds")
+                    for (let i = 0; i < data.length; i++) {
+                        console.log(data[i].URL)
+                        let li = $("<li>").text(data[i].URL)
+                        feeds.append(li)
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+        .catch(err => {
+            console.log(err)
+        })
     // Remove green border when new item is touched
-    let container =  document.getElementById("container")
+    let container = document.getElementById("container")
     container.addEventListener("mouseover", (event) => {
+        event.target.classList.remove("newItem")
+    })
+    container.addEventListener("mouseenter", (event) => {
+        event.target.classList.remove("newItem")
+    })
+    container.addEventListener("mouseenter", (event) => {
         event.target.classList.remove("newItem")
     })
 });
